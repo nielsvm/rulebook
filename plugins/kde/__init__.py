c04ea10e90588c0c4408e4b0261a44835ba4cb8e
Niels van Mourik <niels.vanmourik@acquia.com>
2016-06-13 19:35:15 +0200

    Dropping a PyKDE4 dependency.

--- plugins/kde/__init__.py
+++ plugins/kde/__init__.py
@@ -1,26 +1,32 @@
 __all__ = ['colorscheme', 'notify', 'widget_style']
+from os import popen
 from plugins.kde import *
+from core.action import has_dependency
 from core.path import register_path_prefix, user
-from PyKDE4.kdecore import KStandardDirs
 from core import kde
-
-@register_path_prefix
-def KDEHOME():
-    for path in KStandardDirs().resourceDirs('wallpaper'):
-        if user() in path:
-            return path.replace('/share/wallpapers/', '')
-    return user('.kde')
 
 @register_path_prefix
 def KDEWALLPAPER():
     paths = []
-    for path in KStandardDirs().resourceDirs('wallpaper'):
-        paths.append(path.rstrip('/'))
+    if has_dependency("kf5-config"):
+        wallpaper = popen("kf5-config --path wallpaper")
+        for path in wallpaper.read().split(":"):
+            paths.append(path.rstrip('/'))
+    elif has_dependency("kde4-config"):
+        wallpaper = popen("kde4-config --path wallpaper")
+        for path in wallpaper.read().split(":"):
+            paths.append(path.rstrip('/'))
     return paths
 
 @register_path_prefix
 def KDEDATA():
     paths = []
-    for path in KStandardDirs().resourceDirs('data'):
-        paths.append(path.rstrip('/'))
+    if has_dependency("kf5-config"):
+        data = popen("kf5-config --path data")
+        for path in data.read().split(":"):
+            paths.append(path.rstrip('/'))
+    elif has_dependency("kde4-config"):
+        data = popen("kde4-config --path data")
+        for path in data.read().split(":"):
+            paths.append(path.rstrip('/'))
     return paths
