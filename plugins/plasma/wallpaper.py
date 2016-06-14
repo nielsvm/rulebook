03b16d8c47cbc0902aa0a0df4a974993b7ad971a
Niels van Mourik <niels.vanmourik@acquia.com>
2016-06-14 11:15:30 +0200

    Wallpaper implementation

--- plugins/plasma/wallpaper.py
+++ plugins/plasma/wallpaper.py
@@ -1,4 +1,6 @@
 from plugins.plasma.reload import Reload4, Reload5
+from core import kde, path
+from os import system, popen
 
 class Wallpaper4(Reload4):
     """Set Plasma's wallpaper to the image provided."""
@@ -11,7 +13,7 @@ class Wallpaper4(Reload4):
     def binary_dependencies(self):
         return ['plasma-desktop', 'xdotool']
 
-    def execute(self, theme_name):
+    def execute(self, wallpaper_path):
         tmpfile = '/tmp/plasmawallpaperscript.js'
         if kde.running_dbus('org.kde.plasma-desktop'):
             with open(tmpfile, 'w') as js:
@@ -36,22 +38,32 @@ class Wallpaper5(Reload5):
             ('wallpaper_path', 'Directory containing plasma wallpaper or a file path.'),
         ]
 
-    def binary_dependencies5(self):
-        return ['plasmashell'] #xdotool?
+    def binary_dependencies(self):
+        return ['plasmashell', 'xdotool', 'kwin_x11']
 
-    def execute(self, theme_name):
-        tmpfile = '/tmp/plasmawallpaperscript.js'
-        if kde.running_dbus('org.kde.plasma-desktop'):
-            with open(tmpfile, 'w') as js:
-                js.write("var wallpaper = '%s';\n" % wallpaper_path)
-                js.write("var activity = activities()[0];\n")
-                js.write("activity.currentConfigGroup = new Array('Wallpaper', 'image');\n")
-                js.write("activity.writeConfig('wallpaper', wallpaper);\n")
-                js.write("activity.writeConfig('userswallpaper', wallpaper);\n")
-                js.write("activity.reloadConfig();\n")
-            plasma = kde.get_dbus_object('org.kde.plasma-desktop', '/App')
-            plasma.loadScriptInInteractiveConsole(tmpfile)
-            system('xdotool search --name "Shell Scripting Console" windowactivate key control+e control+w')
-            self.reload()
+    def execute(self, wallpaper_path):
+        if kde.running_dbus('org.kde.plasmashell'):
+            tmpfile = '/tmp/plasmawallpaperscript.js'
+            js = """var activity = currentActivity();
+                var desktops = desktopsForActivity(activity);
+                for (var desktop in desktops) {
+                  desktops[desktop].wallpaperPlugin = 'org.kde.image';
+                  desktops[desktop].wallpaperMode = '';
+                  desktops[desktop].currentConfigGroup = ['Wallpaper', 'org.kde.image', 'General'];
+                  desktops[desktop].writeConfig('Image', wallpaper_path);
+                }"""
+            js = js.replace('  ', '').replace("\n", '  ')
+            js = "wallpaper_path = 'file://%s';%s" % (wallpaper_path, js)
+            # Now lets get hacky, load the script into the console and click buttons.
+            plasma = kde.get_dbus_object('org.kde.plasmashell', '/PlasmaShell')
+            plasma.showInteractiveConsole()
+            mouse_x_y = popen("xdotool getmouselocation --shell")
+            mouse_x_y = mouse_x_y.read().replace("\n", ' ').split(" ")
+            mouse_x_y = "%s %s" % (mouse_x_y[0].replace("X=", ''), mouse_x_y[1].replace("Y=", ''))
+            system('xdotool search --name "Shell Scripting Console" windowactivate')
+            system('xdotool search --name "Shell Scripting Console" key control+a BackSpace')
+            system('xdotool search --name "Shell Scripting Console" windowactivate type "%s"' % js)
+            system('xdotool search --name "Shell Scripting Console" windowactivate mousemove --window %1 370 50 click 1 key control+w')
+            system('xdotool mousemove %s' % mouse_x_y)
             return True
         return False
